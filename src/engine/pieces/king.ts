ba1e2c24652460ba04954811f38efa252caf1ed7
Josh Greenwood <joshgreenwood2003@gmail.com>
2024-06-26 14:25:09 +0100

    King movement

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,13 +1,26 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
-
+import Square from '../square';
 export default class King extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let available = new Array();
+        const currentSquare = board.findPiece(this);
+        
+        available.push(Square.at(currentSquare.row-1,currentSquare.col+1))
+        available.push(Square.at(currentSquare.row-1,currentSquare.col-1))
+        available.push(Square.at(currentSquare.row-1,currentSquare.col))
+        available.push(Square.at(currentSquare.row+1,currentSquare.col+1))
+        available.push(Square.at(currentSquare.row+1,currentSquare.col-1))
+        available.push(Square.at(currentSquare.row+1,currentSquare.col))
+        available.push(Square.at(currentSquare.row,currentSquare.col+1))
+        available.push(Square.at(currentSquare.row,currentSquare.col-1))
+
+
+        return this.reduceMoves(available);
     }
 }
