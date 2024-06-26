2fe4172fd9bcbb570a7546579bf94d3da4d086cf
Josh Greenwood <joshgreenwood2003@gmail.com>
2024-06-26 11:54:05 +0100

    basic rook moves

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,13 +1,26 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
-
+import Square from '../square';
 export default class Rook extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let available = new Array();
+        const currentSquare = board.findPiece(this);
+        for(let i = 0;i<8;i++){
+            if (i != currentSquare.row){
+                available.push(Square.at(i,currentSquare.col))
+            }     
+        }
+        for(let i = 0;i<8;i++){
+            if (i != currentSquare.col){
+                available.push(Square.at(currentSquare.row,i))
+            }     
+        }
+
+        return available;
     }
 }
