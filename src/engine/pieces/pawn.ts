b4b9d45082753d99fac959390d3baf8473322de5
Josh Greenwood <joshgreenwood2003@gmail.com>
2024-06-26 14:44:38 +0100

    Pawns blocked by infront pieces

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -10,20 +10,36 @@ export default class Pawn extends Piece {
 
     public getAvailableMoves(board: Board) {
         let available = new Array();
+
         const currentSquare = board.findPiece(this);
-        if (this.player == Player.WHITE){
-            if (currentSquare.row == 1){
-                available.push(Square.at(currentSquare.row+2,currentSquare.col))
-            }
-            available.push(Square.at(currentSquare.row+1,currentSquare.col))
-        }
-        else{
-            if (currentSquare.row == 6){
-                available.push(Square.at(currentSquare.row-2,currentSquare.col))
-            }
-            available.push(Square.at(currentSquare.row-1,currentSquare.col))
-        }
         
+        if (this.player == Player.WHITE){
+            let nextSquare = Square.at(currentSquare.row + 1,currentSquare.col)
+            let secondNextSquare = Square.at(currentSquare.row + 2,currentSquare.col)
+            if (currentSquare.row == 1){
+                if (typeof board.getPiece(nextSquare) == "undefined" && typeof board.getPiece(secondNextSquare) == "undefined"){
+                    available.push(secondNextSquare)
+                }  
+            }
+            if (typeof board.getPiece(nextSquare) == "undefined" && typeof board.getPiece(secondNextSquare) == "undefined"){
+                available.push(nextSquare)
+            }  
+        }
+
+
+        if (this.player == Player.BLACK){
+            let nextSquare = Square.at(currentSquare.row - 1,currentSquare.col)
+            let secondNextSquare = Square.at(currentSquare.row -2,currentSquare.col)
+            if (currentSquare.row == 6){
+                if (typeof board.getPiece(nextSquare) == "undefined" && typeof board.getPiece(secondNextSquare) == "undefined"){
+                    available.push(secondNextSquare)
+                }  
+            }
+            if (typeof board.getPiece(nextSquare) == "undefined" && typeof board.getPiece(secondNextSquare) == "undefined"){
+                available.push(nextSquare)
+            }  
+        }
+
         return this.reduceMoves(available);
     }
 }
