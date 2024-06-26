0b6c11defe8415d13661b1937fb4aa42a07da43f
Josh Greenwood <joshgreenwood2003@gmail.com>
2024-06-26 16:41:07 +0100

    rooks can take opposing

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -14,10 +14,14 @@ export default class Rook extends Piece {
         let blocked = false;
         for(let i = currentSquare.col - 1; i >= 0; i--){
             if (!blocked){
-                if (typeof board.getPiece(Square.at(currentSquare.row,i)) === "undefined"){
+                let piece = board.getPiece(Square.at(currentSquare.row,i))
+                if (typeof piece === "undefined"){
                     available.push(Square.at(currentSquare.row,i));
                 }
                 else{
+                    if(piece.player != this.player&& piece.constructor.name != "King"){
+                        available.push(Square.at(currentSquare.row,i)); 
+                    }
                     blocked = true;
                 }
 
@@ -27,10 +31,14 @@ export default class Rook extends Piece {
         blocked = false;
         for(let i = currentSquare.col +1; i <=7 ; i++){
             if (!blocked){
-                if (typeof board.getPiece(Square.at(currentSquare.row,i)) === "undefined"){
+                let piece = board.getPiece(Square.at(currentSquare.row,i))
+                if (typeof piece === "undefined"){
                     available.push(Square.at(currentSquare.row,i));
                 }
                 else{
+                    if(piece.player != this.player&& piece.constructor.name != "King"){
+                        available.push(Square.at(currentSquare.row,i)); 
+                    }
                     blocked = true;
                 }
 
@@ -42,10 +50,14 @@ export default class Rook extends Piece {
         blocked = false;
         for(let i = currentSquare.row - 1; i >= 0; i--){
             if (!blocked){
-                if (typeof board.getPiece(Square.at(i,currentSquare.col)) === "undefined"){
+                let piece = board.getPiece(Square.at(i,currentSquare.col))
+                if (typeof piece === "undefined"){
                     available.push(Square.at(i,currentSquare.col));
                 }
                 else{
+                    if(piece.player != this.player&& piece.constructor.name != "King"){
+                        available.push(Square.at(i,currentSquare.col)); 
+                    }
                     blocked = true;
                 }
 
@@ -55,10 +67,14 @@ export default class Rook extends Piece {
         blocked = false;
         for(let i = currentSquare.row +1; i <=7 ; i++){
             if (!blocked){
-                if (typeof board.getPiece(Square.at(i,currentSquare.col)) === "undefined"){
+                let piece = board.getPiece(Square.at(i,currentSquare.col))
+                if (typeof piece === "undefined"){
                     available.push(Square.at(i,currentSquare.col));
                 }
                 else{
+                    if(piece.player != this.player && piece.constructor.name != "King"){
+                        available.push(Square.at(i,currentSquare.col)); 
+                    }
                     blocked = true;
                 }
 
