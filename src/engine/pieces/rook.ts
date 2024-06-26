4bf31378289759544a01bf02674de7c58ef5494a
Josh Greenwood <joshgreenwood2003@gmail.com>
2024-06-26 14:56:27 +0100

    rook cannot collide

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -10,14 +10,62 @@ export default class Rook extends Piece {
     public getAvailableMoves(board: Board) {
         let available = new Array();
         const currentSquare = board.findPiece(this);
-        for(let i = 0;i<8;i++){
-            if (i != currentSquare.row){
-                available.push(Square.at(i,currentSquare.col))
-            }    
-            if (i != currentSquare.col){
-                available.push(Square.at(currentSquare.row,i))
-            }      
+
+        let blocked = false;
+        for(let i = currentSquare.col - 1; i >= 0; i--){
+            if (!blocked){
+                if (typeof board.getPiece(Square.at(currentSquare.row,i)) === "undefined"){
+                    available.push(Square.at(currentSquare.row,i));
+                }
+                else{
+                    blocked = true;
+                }
+
+            }
+
         }
+        blocked = false;
+        for(let i = currentSquare.col +1; i <=7 ; i++){
+            if (!blocked){
+                if (typeof board.getPiece(Square.at(currentSquare.row,i)) === "undefined"){
+                    available.push(Square.at(currentSquare.row,i));
+                }
+                else{
+                    blocked = true;
+                }
+
+            }
+
+        }
+
+
+        blocked = false;
+        for(let i = currentSquare.row - 1; i >= 0; i--){
+            if (!blocked){
+                if (typeof board.getPiece(Square.at(i,currentSquare.col)) === "undefined"){
+                    available.push(Square.at(i,currentSquare.col));
+                }
+                else{
+                    blocked = true;
+                }
+
+            }
+
+        }
+        blocked = false;
+        for(let i = currentSquare.row +1; i <=7 ; i++){
+            if (!blocked){
+                if (typeof board.getPiece(Square.at(i,currentSquare.col)) === "undefined"){
+                    available.push(Square.at(i,currentSquare.col));
+                }
+                else{
+                    blocked = true;
+                }
+
+            }
+
+        }
+      
         return available;
     }
 }
