8a8cda5dd84105613d0f10eb0b92fc4de556dd0d
Josh Greenwood <joshgreenwood2003@gmail.com>
2024-06-26 17:06:27 +0100

    Queen works correctly

--- src/engine/pieces/piece.ts
+++ src/engine/pieces/piece.ts
@@ -27,4 +27,173 @@ export default class Piece {
         });
         return newMoves
     }
+
+
+
+
+
+
+
+    public getVerticalMoves(board: Board) {
+        let available = new Array();
+        const currentSquare = board.findPiece(this);
+
+        let blocked = false;
+        for(let i = currentSquare.col - 1; i >= 0; i--){
+            if (!blocked){
+                let piece = board.getPiece(Square.at(currentSquare.row,i))
+                if (typeof piece === "undefined"){
+                    available.push(Square.at(currentSquare.row,i));
+                }
+                else{
+                    if(piece.player != this.player&& piece.constructor.name != "King"){
+                        available.push(Square.at(currentSquare.row,i)); 
+                    }
+                    blocked = true;
+                }
+
+            }
+
+        }
+        blocked = false;
+        for(let i = currentSquare.col +1; i <=7 ; i++){
+            if (!blocked){
+                let piece = board.getPiece(Square.at(currentSquare.row,i))
+                if (typeof piece === "undefined"){
+                    available.push(Square.at(currentSquare.row,i));
+                }
+                else{
+                    if(piece.player != this.player&& piece.constructor.name != "King"){
+                        available.push(Square.at(currentSquare.row,i)); 
+                    }
+                    blocked = true;
+                }
+
+            }
+
+        }
+
+
+        blocked = false;
+        for(let i = currentSquare.row - 1; i >= 0; i--){
+            if (!blocked){
+                let piece = board.getPiece(Square.at(i,currentSquare.col))
+                if (typeof piece === "undefined"){
+                    available.push(Square.at(i,currentSquare.col));
+                }
+                else{
+                    if(piece.player != this.player&& piece.constructor.name != "King"){
+                        available.push(Square.at(i,currentSquare.col)); 
+                    }
+                    blocked = true;
+                }
+
+            }
+
+        }
+        blocked = false;
+        for(let i = currentSquare.row +1; i <=7 ; i++){
+            if (!blocked){
+                let piece = board.getPiece(Square.at(i,currentSquare.col))
+                if (typeof piece === "undefined"){
+                    available.push(Square.at(i,currentSquare.col));
+                }
+                else{
+                    if(piece.player != this.player && piece.constructor.name != "King"){
+                        available.push(Square.at(i,currentSquare.col)); 
+                    }
+                    blocked = true;
+                }
+
+            }
+
+        }
+      
+        return available;
+    }
+
+
+
+    public getDiagonalMoves(board: Board) {
+        let available = new Array();
+        const currentSquare = board.findPiece(this);
+
+        let blockNE = false;
+        let blockNW = false;
+        for (let row = currentSquare.row + 1; row < 8; row++) {
+            if (row != currentSquare.row) {
+                let d1pos = row - currentSquare.row + currentSquare.col;
+                let d2pos = currentSquare.row + currentSquare.col - row;
+                let possibleSquare = Square.at(row, d1pos)
+                let possibleSquare2 = Square.at(row, d2pos)
+                if (blockNE == false) {
+
+                    let piece = board.getPiece(possibleSquare)
+                    if (typeof piece === "undefined") {
+                        available.push(possibleSquare);
+                    }
+                    else {
+                        if (piece.player != this.player && piece.constructor.name != "King") {
+                            available.push(possibleSquare);
+                        }
+                        blockNE = true;
+                    }
+
+                }
+                if (blockNW == false) {
+                    let piece = board.getPiece(possibleSquare2)
+                    if (typeof piece === "undefined") {
+                        available.push(possibleSquare2);
+                    }
+                    else {
+                        if (piece.player != this.player && piece.constructor.name != "King") {
+                            available.push(possibleSquare2);
+                        }
+                        blockNW = true;
+                    }
+
+                }
+            }
+        }
+        let blockSW = false;
+        let blockSE = false;
+        for (let row = currentSquare.row - 1; row >= 0; row--) {
+            if (row != currentSquare.row) {
+                let d1pos = row - currentSquare.row + currentSquare.col;
+                let d2pos = currentSquare.row + currentSquare.col - row;
+                let possibleSquare = Square.at(row, d1pos)
+                let possibleSquare2 = Square.at(row, d2pos)
+                if (blockSW == false) {
+                    let piece = board.getPiece(possibleSquare)
+                    if (typeof piece === "undefined") {
+                        available.push(possibleSquare);
+                    }
+                    else {
+                        if (piece.player != this.player && piece.constructor.name != "King") {
+                            available.push(possibleSquare);
+                        }
+                        blockSW = true;
+                    }
+
+                }
+                if (blockSE == false) {
+                    let piece = board.getPiece(possibleSquare2)
+                    if (typeof piece === "undefined") {
+                        available.push(possibleSquare2);
+                    }
+                    else {
+                        if (piece.player != this.player && piece.constructor.name != "King") {
+                            available.push(possibleSquare2);
+                        }
+                        blockSE = true;
+                    }
+                }
+            }
+        }
+        available = this.reduceMoves(available)
+        return available;
+    }
+
+
+
 }
--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -2,6 +2,9 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import Bishop from './bishop';
+import Rook from './rook';
+
 export default class Queen extends Piece {
     public constructor(player: Player) {
         super(player);
@@ -9,124 +12,8 @@ export default class Queen extends Piece {
 
     public getAvailableMoves(board: Board) {
 
-
-
-        let available = new Array();
-        const currentSquare = board.findPiece(this);
-
-        let blocked = false;
-        for(let i = currentSquare.col - 1; i >= 0; i--){
-            if (!blocked){
-                if (typeof board.getPiece(Square.at(currentSquare.row,i)) === "undefined"){
-                    available.push(Square.at(currentSquare.row,i));
-                }
-                else{
-                    blocked = true;
-                }
-
-            }
-
-        }
-        blocked = false;
-        for(let i = currentSquare.col +1; i <=7 ; i++){
-            if (!blocked){
-                if (typeof board.getPiece(Square.at(currentSquare.row,i)) === "undefined"){
-                    available.push(Square.at(currentSquare.row,i));
-                }
-                else{
-                    blocked = true;
-                }
-
-            }
-
-        }
-
-
-        blocked = false;
-        for(let i = currentSquare.row - 1; i >= 0; i--){
-            if (!blocked){
-                if (typeof board.getPiece(Square.at(i,currentSquare.col)) === "undefined"){
-                    available.push(Square.at(i,currentSquare.col));
-                }
-                else{
-                    blocked = true;
-                }
-
-            }
-
-        }
-        blocked = false;
-        for(let i = currentSquare.row +1; i <=7 ; i++){
-            if (!blocked){
-                if (typeof board.getPiece(Square.at(i,currentSquare.col)) === "undefined"){
-                    available.push(Square.at(i,currentSquare.col));
-                }
-                else{
-                    blocked = true;
-                }
-
-            }
-
-        }
-
-
-
-        let blockNE = false;
-        let blockNW= false;
-        for(let row = currentSquare.row + 1 ;row<8;row++){
-            if (row != currentSquare.row){
-                let d1pos = row-currentSquare.row+currentSquare.col;
-                let d2pos = currentSquare.row+currentSquare.col-row;
-                let possibleSquare = Square.at(row,d1pos)
-                let possibleSquare2 = Square.at(row,d2pos)
-               if(blockNE == false){
-                if (typeof board.getPiece(possibleSquare) === "undefined"){
-                    available.push(possibleSquare)
-                }
-                else{
-                    blockNE = true
-                }
-
-               }
-               if (blockNW == false){
-                if (typeof board.getPiece(possibleSquare2) === "undefined"){
-                    available.push(possibleSquare2)
-                }
-                else{
-                    blockNW = true
-                }
-               }
-               
-            }     
-        }
-        let blockSW = false;
-        let blockSE = false;
-        for(let row = currentSquare.row -1 ;row>=0;row--){
-            if (row != currentSquare.row){
-                let d1pos = row-currentSquare.row+currentSquare.col;
-                let d2pos = currentSquare.row+currentSquare.col-row;
-                let possibleSquare = Square.at(row,d1pos)
-                let possibleSquare2 = Square.at(row,d2pos)
-               if(blockSW == false){
-                if (typeof board.getPiece(possibleSquare) === "undefined"){
-                    available.push(possibleSquare)
-                }
-                else{
-                    blockSW = true
-                }
-
-               }
-               if (blockSE == false){
-                if (typeof board.getPiece(possibleSquare2) === "undefined"){
-                    available.push(possibleSquare2)
-                }
-                else{
-                    blockSE = true
-                }
-               }
-            }       
-        }
-        available = this.reduceMoves(available)
+        let available= this.getDiagonalMoves(board)
+        available = available.concat(this.getVerticalMoves(board))
         return available;
       
     }
