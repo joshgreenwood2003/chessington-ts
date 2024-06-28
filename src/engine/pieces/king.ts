5567f4492e17e5e92ca3aab4fc0d72a92302958c
Josh Greenwood <joshgreenwood2003@gmail.com>
2024-06-28 17:08:06 +0100

    castling

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -2,6 +2,8 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import Rook from './rook';
+import { updateChessBoard } from '../../frontend/js/chessington';
 export default class King extends Piece {
     public constructor(player: Player) {
         super(player);
@@ -16,6 +18,50 @@ export default class King extends Piece {
             board.CastleKsW = false;
             board.CastleQsW = false;
         }
+
+
+
+        if(fromSquare.col == 4 && toSquare.col == 6){
+            if (this.player == Player.BLACK){
+                let rook = board.getPiece(new Square(7,7));
+                board.setPiece(new Square(7,7),undefined);
+                board.setPiece(new Square(7,5),rook);
+                updateChessBoard();
+
+            }
+            else{
+                let rook = board.getPiece(new Square(0,7));
+                board.setPiece(new Square(0,7),undefined);
+                board.setPiece(new Square(0,5),rook);
+                updateChessBoard();
+            }
+        }
+        
+        if(fromSquare.col == 4 && toSquare.col == 2){
+            if (this.player == Player.BLACK){
+                let rook = board.getPiece(new Square(7,0));
+                board.setPiece(new Square(7,0),undefined);
+                board.setPiece(new Square(7,3),rook);
+                updateChessBoard();
+
+            }
+            else{
+                let rook = board.getPiece(new Square(0,0));
+                board.setPiece(new Square(0,0),undefined);
+                board.setPiece(new Square(0,3),rook);
+                updateChessBoard();
+            }
+        }
+       
+
+
+
+
+
+
+
+
+
         return;
           
     }
@@ -33,6 +79,33 @@ export default class King extends Piece {
         available.push(new Square(currentRow + 1, currentCol));
         available.push(new Square(currentRow, currentCol + 1));
         available.push(new Square(currentRow, currentCol - 1));
+
+
+        if (this.player === Player.BLACK){
+            if(board.CastleKsB){
+                if (!board.getPiece(new Square(7,5))&&!board.getPiece(new Square(7,6))){
+                    available.push(new Square(7,6));
+                }
+            }
+            if(board.CastleQsB){
+                if (!board.getPiece(new Square(7,3))&&!board.getPiece(new Square(7,2))&& !board.getPiece(new Square(7,1))){
+                    available.push(new Square(7,2));
+                }
+            }
+        }
+        else{
+            if(board.CastleKsW){
+                if (!board.getPiece(new Square(0,5))&&!board.getPiece(new Square(0,6))){
+                    available.push(new Square(0,6));
+                }
+            }
+            if(board.CastleQsW){
+                if (!board.getPiece(new Square(0,3))&&!board.getPiece(new Square(0,2))&& !board.getPiece(new Square(0,1))){
+                    available.push(new Square(0,2));
+                }
+            }
+        }
+        
 
         return this.reduceMoves(board, available);
     }
